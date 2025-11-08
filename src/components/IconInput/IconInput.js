cac5a201959ed6eb0a8e737c28d61241b8a9fad3
JNeo77 <lifted_2000@yahoo.com>
2025-11-07 19:17:02 -0500

    Workshop complete

--- src/components/IconInput/IconInput.js
+++ src/components/IconInput/IconInput.js
@@ -6,6 +6,21 @@ import { COLORS } from '../../constants';
 import Icon from '../Icon';
 import VisuallyHidden from '../VisuallyHidden';
 
+const SIZES = {
+  small: {
+    '--fontSize': 0.875 + 'rem',
+    '--padding': `2px 0 2px 24px`,
+    '--iconSize': 16 + 'px',
+    '--bottom': 2 + 'px'
+  },
+  large: {
+    '--fontSize': 1.125 + 'rem',
+    '--padding': `4px 0 4px 32px`,
+    '--iconSize': 24 + 'px',
+    '--bottom': 4 + 'px'
+  }
+}
+
 const IconInput = ({
   label,
   icon,
@@ -13,7 +28,54 @@ const IconInput = ({
   size,
   placeholder,
 }) => {
-  return 'TODO';
+  const styles = SIZES[size]
+  return (
+    <Wrapper>
+      <VisuallyHidden>{label}</VisuallyHidden>
+      <SearchInput style={{'--width': width + 'px', ...styles}} placeholder={placeholder}></SearchInput>
+      <IconWrapper style={styles}>
+        <Icon id={icon} stroke-width={1} size={size === 'large' ? 24 : 16}></Icon>
+      </IconWrapper>
+    </Wrapper>
+  );
 };
+
+const Wrapper = styled.div`
+  position: relative;
+  width: max-content;
+  border-bottom: 2px solid black;
+`
+
+const SearchInput = styled.input`
+  width: var(--width);
+  border: none;
+  font-size: var(--fontSize);
+  padding: var(--padding);
+  font-weight: bold;
+  color: ${COLORS.gray700};
+
+  &::placeholder {
+    color: ${COLORS.gray500};
+    font-weight: 400;
+  }
+
+  &:focus {
+    outline-offset: 4px;
+  }
+
+  &:hover {
+    color: black;
+  }
+`
+
+const IconWrapper = styled.div`
+  position: absolute;
+  left: 0;
+  bottom: var(--bottom);
+  width: var(--iconSize);
+  height: var(--iconSize);
+  margin: auto;
+  pointer-events: none;
+`
 
 export default IconInput;
--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -5,8 +5,62 @@ import styled from 'styled-components';
 import { COLORS } from '../../constants';
 import VisuallyHidden from '../VisuallyHidden';
 
+const SIZES = {
+  small: {
+    '--borderRadius': 4 + 'px',
+    '--barHeight': 8 + 'px',
+    '--height': 8 + 'px'
+  },
+  medium: {
+    '--borderRadius': 4 + 'px',
+    '--barHeight': 12 + 'px',
+    '--height': 12 + 'px'
+  },
+  large: {
+    '--borderRadius': 8 + 'px',
+    '--height': 24 + 'px',
+    '--barHeight': 16 + 'px',
+    '--padding': 4 + 'px'
+  }
+}
+
 const ProgressBar = ({ value, size }) => {
-  return <strong>{value}</strong>;
+  const styles = SIZES[size]
+  return <Wrapper style={styles} value={value} max={100}></Wrapper>;
 };
+
+const Wrapper = styled.progress`
+  accent-color: ${COLORS.primary};
+  background-color: ${COLORS.transparentGray15};
+  -webkit-appearance: none;
+  box-shadow: inset 0px 2px 4px ${COLORS.transparentGray35};
+  border-radius: var(--borderRadius);
+  padding: var(--padding);
+  height: var(--height);
+  width: 370px;
+
+  &::-webkit-progress-bar {
+    background-color: transparent;
+    height: var(--barHeight);
+  }
+
+  &::-webkit-progress-value {
+    background-color: ${COLORS.primary};
+    border-radius: ${props => {
+        if (props.value === 99) {
+          return '4px 2px 2px 4px'
+        } else if (props.value === 100) {
+          return '4px'
+        } else {
+          return '4px 0 0 4px'
+        }
+      }
+    }
+  }
+
+  &::-moz-progress-bar {
+    background-color: ${COLORS.primary};
+  }
+`;
 
 export default ProgressBar;
